741021de5a29a90fa4c34c810320a392e97b9016
Chris Brownlie <chris.brownlie@hotmail.co.uk>
2022-04-11 09:16:32 +0100

    feat: page title

--- R/app_ui.R
+++ R/app_ui.R
@@ -3,6 +3,7 @@ app_ui <- function() {
   
   # UI definition
   ui <- fluidPage(
+    title = "scriptSearch|chrisbrownlie.com",
     tags$head(
       # Include custom CSS
       tags$link(href = "styling.css", rel = "stylesheet", type = "text/css"),
